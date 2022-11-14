20aaf2ba2e7a10e3983257f5df26f048d08b8c80
Robert McNees <rmcnees@vmware.com>
2022-11-14 12:22:08 -0500

    Initial commit of code for guide

---
+++ complete/task/src/main/java/com/example/task/TaskApplication.java
@@ -0,0 +1,46 @@
+package com.example.task;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.cloud.task.listener.annotation.AfterTask;
+import org.springframework.cloud.task.listener.annotation.BeforeTask;
+import org.springframework.cloud.task.repository.TaskExecution;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+@EnableTask
+public class TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return new HelloWorldCommandLineRunner();
+	}
+
+	@BeforeTask
+	public void beforeMethod(TaskExecution taskExecution) {
+		System.out.println("Before task");
+	}
+
+	@AfterTask
+	public void afterMethod(TaskExecution taskExecution) {
+		System.out.println("After task");
+	}
+
+	public static class HelloWorldCommandLineRunner implements CommandLineRunner {
+
+		@Override
+		public void run(String... args) throws Exception {
+			System.out.println("Hello Spring Cloud Task!");
+		}
+	}
+}
